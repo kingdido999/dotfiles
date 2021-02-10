d61fbbe85b24961a6b36c90fec83eb6e1e95b3bd
Pengcheng Ding <info@desmonding.me>
2021-02-10 15:15:42 -0800

    Set PATH in emacs

--- emacs/.doom.d/config.el
+++ emacs/.doom.d/config.el
@@ -68,3 +68,11 @@
 
 ;; vterm
 (setq vterm-shell "/bin/zsh")
+
+;; set PATH
+(let ((path (shell-command-to-string ". ~/.zshrc; echo -n $PATH")))
+  (setenv "PATH" path)
+  (setq exec-path
+        (append
+         (split-string-and-unquote path ":")
+         exec-path)))
