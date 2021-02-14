0106caaaea0e2eccb53c73bacb2eec3605250634
Pengcheng Ding <info@desmonding.me>
2021-02-13 16:39:17 -0800

    Add setup script

---
+++ setup.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -euox pipefail
+
+if ! command -v brew &> /dev/null
+then
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+brew install bat tmux ripgrep fd
+
+if [ ! -d "~/.oh-my-zsh" ]; then
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
+
+if [ ! -d "~/.oh-my-zsh/custom/plugins/zsh-autosuggestions" ]; then
+    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+fi
