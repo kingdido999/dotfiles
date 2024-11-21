794631332cb7c5feb520962119997b505e747bc7
Pengcheng Ding <info@pcding.com>
2024-11-20 16:02:38 -0800

    Update .config/home-manager/home.nix

--- dot_config/home-manager/home.nix
+++ dot_config/home-manager/home.nix
@@ -91,4 +91,14 @@
       { name = "hydro"; src = pkgs.fishPlugins.hydro.src; }
     ];
   };
+
+  programs.bash = {
+    interactiveShellInit = ''
+      if [[ $(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "fish" && -z ''${BASH_EXECUTION_STRING} ]]
+      then
+        shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
+        exec ${pkgs.fish}/bin/fish $LOGIN_OPTION
+      fi
+    '';
+  };
 }
